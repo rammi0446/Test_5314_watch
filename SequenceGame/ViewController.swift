a1971257e52f65e9cda888440081db3949b0249f
ramandeep <rammi.0446@gmail.com>
2019-07-10 09:14:58 -0400

    initial commit

---
+++ SequenceGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SequenceGame
+//
+//  Created by MacStudent on 2019-07-10.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
