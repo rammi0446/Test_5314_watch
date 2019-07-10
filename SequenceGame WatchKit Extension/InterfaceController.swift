def3d7ac36ce2f75f64e7f527e7e9c70204a30b0
ramandeep <rammi.0446@gmail.com>
2019-07-10 10:17:57 -0400

    enter sequence on phone side

--- SequenceGame WatchKit Extension/InterfaceController.swift
+++ SequenceGame WatchKit Extension/InterfaceController.swift
@@ -11,6 +11,14 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    var loading_1: UIImage!
+    var loading_2: UIImage!
+    var loading_3: UIImage!
+    
+    var images: [UIImage]!
+ 
+    
+    var animatedImage: UIImage!
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -21,6 +29,17 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        
+//        loading_1 = UIImage(named: "fish")
+//        loading_2 = UIImage(named: "sun")
+//        loading_3 = UIImage(named: "sunglasses")
+//        
+////        images = [loading_1!, loading_2!, loading_3!]
+//        
+//        animatedImage = UIImage.animatedImage(with: images, duration: 1.0)
+       
+        //ImageView.image = animatedImage
     }
     
     override func didDeactivate() {
--- SequenceGame/ViewController.swift
+++ SequenceGame/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func fish(_ sender: Any) {
+        print("you select fish")
+    }
+    
+    @IBAction func sun(_ sender: Any) {
+        print("you select sun")
+    }
+    @IBAction func sunglasses(_ sender: Any) {
+        print("you select sunglasses")
+    }
+    @IBAction func watermelon(_ sender: Any) {
+        print("you select watermelon")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
